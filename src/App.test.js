0ce5568bc4d3eb983958eee28e6141cc56cadc92
Dariusz Okoński <darek200180@gmail.com>
2022-11-24 16:07:53 +0100

    add some tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,27 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+// import { render, screen } from "@testing-library/react";
+// import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText("Learn React");
-  expect(linkElement).toBeInTheDocument();
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = screen.getByText("Learn React");
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+let myValue = 1;
+
+function add(x, y) {
+  return x + y;
+}
+
+test("First", () => {
+  expect(add(4, 5)).toBe(9);
 });
 
-test("second", () => {});
+test.only("Second", () => {
+  expect(myValue).toBe(1);
+});
+
+test("check add", () => {
+  expect(add(1, 2)).toBe(3);
+  expect(add(4, 5)).toBe(9);
+});
